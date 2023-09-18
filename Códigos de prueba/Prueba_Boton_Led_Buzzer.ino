ea48c0bec80cce120abfcc5a08e306061b565bac
isabellamastrangelo <95829691+isabellamastrangelo@users.noreply.github.com>
2023-09-18 20:27:14 -0300

    Update Prueba_Boton_Led_Buzzer.ino

--- Códigos de prueba/Prueba_Boton_Led_Buzzer.ino
+++ Códigos de prueba/Prueba_Boton_Led_Buzzer.ino
@@ -1,13 +1,17 @@
-#define apretado 1
-#define no_apretado 0
-#define boton1 33
+#define apretado 0
+#define no_apretado 1
+#define boton1 27 //33: abajo(funciona al revez), 32:atras , 25: arriba, 26: adelante , 27: aceptar
 #define led1 14
+#define led2 13
+#define led3 12
 #define buzzer 12
 int estadoBoton1;
 
 void setup() {
-  pinMode(boton1, INPUT);
+  pinMode(boton1, INPUT_PULLUP);
   pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
   pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
 }
@@ -18,8 +22,12 @@ void loop() {
     Serial.println("Boton 1 apretado");
     digitalWrite(buzzer, HIGH);
     digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
   } else {
     digitalWrite(buzzer, LOW);
     digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
   }
 }
